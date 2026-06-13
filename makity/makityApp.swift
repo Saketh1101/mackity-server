9d83ba5c9978a7d2b9cf9de253379f184f071ba3
Saketh <sakethginne@gmail.com>
2026-06-13 10:34:31 +0530

    Initial Commit

---
+++ makity/makityApp.swift
@@ -0,0 +1,17 @@
+//
+//  makityApp.swift
+//  makity
+//
+//  Created by Saketh on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct makityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
